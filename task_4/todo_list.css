7269a29adb746aa0d7ccbf2aab324dc3b2cc769f
kudali-ram <160263341+kudali-ram@users.noreply.github.com>
2024-06-16 08:32:06 +0530

    Add files via upload

---
+++ task_4/todo_list.css
@@ -0,0 +1,87 @@
+/* src/TodoList.css */
+.todo-list {
+    width: 400px;
+    margin: 50px auto;
+    padding: 20px;
+    border: 2px solid #b63b3b;
+    border-radius: 10px;
+    background-color: #e3f2fd;
+    box-shadow: 0 5px 15px rgba(97, 74, 74, 0.2);
+    text-align: center;
+    font-family: 'Verdana', sans-serif;
+  }
+  
+  h1 {
+    margin-bottom: 20px;
+    color: #1d87c5;
+    font-family: 'Georgia', serif;
+  }
+  
+  .input-container {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+  
+  input[type="text"] {
+    padding: 10px;
+    font-size: 16px;
+    margin-right: 10px;
+    width: 60%;
+    border: 1px solid #bbb;
+    border-radius: 5px;
+  }
+  
+  button {
+    padding: 10px 15px;
+    font-size: 16px;
+    color: #fff;
+    background-color: #0cd416;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    background-color: #264b28;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    border-bottom: 1px solid #cf1515;
+  }
+  
+  li:last-child {
+    border-bottom: none;
+  }
+  
+  li.completed span {
+    text-decoration: line-through;
+    color: #885555;
+  }
+  
+  li input[type="checkbox"] {
+    margin-right: 10px;
+  }
+  
+  li button {
+    background-color: #d32e2e;
+    color: white;
+    border: none;
+    padding: 5px 10px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+  li button:hover {
+    background-color: #c62828;
+  }
